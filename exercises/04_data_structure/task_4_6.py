9d121038ede73f7f7201aafd29fc27b6413afc90
Ruslan Fomin <rusnino@gmail.com>
2019-01-27 22:33:02 +0300

    added task 4.6

--- exercises/04_data_structure/task_4_6.py
+++ exercises/04_data_structure/task_4_6.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 '''
 Задание 4.6
@@ -15,3 +16,29 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+print(ospf_route)
+list_string_ospf = ospf_route.split()
+
+output_string_template = '''
+{0:<24}{6:<24}
+{1:<24}{7:<24}
+{2:<24}{8:<24}
+{3:<24}{9:<24}
+{4:<24}{10:<24}
+{5:<24}{11:<24}
+'''
+
+print(output_string_template.format('Protocol:',
+									'Prefix:', 
+									'AD/Metric:', 
+									'Next-Hop:', 
+									'Last update:', 
+									'Outbound Interface:', 
+									list_string_ospf[0], 
+									list_string_ospf[1], 
+									list_string_ospf[2].strip('[]'), 
+									list_string_ospf[4].strip(','), 
+									list_string_ospf[5].strip(','), 
+									list_string_ospf[6]
+									)
+)
