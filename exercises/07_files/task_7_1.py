00aba5654c4f08d73c40f18df375a62849789108
Ruslan Fomin <rusnino@gmail.com>
2019-02-04 22:01:16 +0300

    add 7.1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 '''
 Задание 7.1
@@ -14,3 +15,32 @@ Outbound Interface:    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+#ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+with open('ospf.txt', 'r') as f:
+    for ospf_route in f:
+    #    print(ospf_route)
+        list_string_ospf = ospf_route.split()
+        
+        output_string_template = '''
+        {0:<24}{6:<24}
+        {1:<24}{7:<24}
+        {2:<24}{8:<24}
+        {3:<24}{9:<24}
+        {4:<24}{10:<24}
+        {5:<24}{11:<24}
+        '''
+        
+        print(output_string_template.format('Protocol:',
+        									'Prefix:', 
+        									'AD/Metric:', 
+        									'Next-Hop:', 
+        									'Last update:', 
+        									'Outbound Interface:', 
+        									list_string_ospf[0]+'SPF', 
+        									list_string_ospf[1], 
+        									list_string_ospf[2].strip('[]'), 
+        									list_string_ospf[4].strip(','), 
+        									list_string_ospf[5].strip(','), 
+        									list_string_ospf[6]
+        									)
+        )
