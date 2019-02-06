310c7328ff202a8999931694c90a93e8d1d7d905
Ruslan Fomin <rusnino@gmail.com>
2019-02-07 02:08:09 +0300

    autocomment

--- examples/08_python_basic_examples/working_with_dict_example_1.py
+++ examples/08_python_basic_examples/working_with_dict_example_1.py
@@ -3,6 +3,7 @@ result = {}
 with open('sh_ip_int_br.txt') as f:
     for line in f:
         line = line.split()
+        print(line)
         if line and line[1][0].isdigit():
             interface, address, *other = line
             result[interface] = address
